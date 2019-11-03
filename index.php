6f51adf7731b022a172a83cdde4ba1744c9791e4
Philip <p.janssen@posteo.de>
2019-11-03 13:30:56 +0100

    upgrade page
    - css grid instead of tables
    - fancy link animation
    - impressum

--- index.php
+++ index.php
@@ -1,20 +1,53 @@
 <html>
-
 	<head>
-		<title>Viva La Luz</title>
+		<title>vivalaluz.net</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
-
-	<frameset rows="40,*,30" border="0" frameborder="no" framespacing="0">
-
-		<frame name="top" src="head.php" scrolling="no" marginwidth="0" marginheight="0">
-		<frame name="main" src="main.php" marginwidth="0" marginheight="0">
-		<frame name="bottom" src="bottom.php" scrolling="no" marginwidth="0" marginheight="0">
-		<noframes>
-
-			<body></body>
-
-		</noframes>
-	</frameset>
-
+	<body>
+		<div class="container">
+		<div class="header">
+			<h1 class="link"><a href="/">vivalaluz</a></h1>
+		</div>
+		<div class="main">
+			<?php
+					$root = "content";
+					if (file_exists($root))
+						{
+						$albums = scandir($root);
+						sort($albums);
+						$count = 1;
+						foreach ($albums as $album)
+						{
+							$albumPath = $root."/".$album;
+							if(is_dir($albumPath) && $album != "." && $album != "..")
+							{
+								$albumName = preg_replace("~_~", "/", $album);
+								$albumName = preg_replace("~-~", " ", $albumName);
+								echo "<p>$albumName<p>";
+								$pics = scandir($albumPath);
+								foreach ($pics as $pic)
+								{
+									if(preg_match('/.jpg/', $pic) OR preg_match('/.png/', $pic))
+									{
+										if($pic != "." && $pic != ".." &&  !is_dir($pic))
+										{
+											echo "<td><img src=$albumPath/$pic border='0' height='450'>&nbsp;</td>";
+											if($count%2==0)
+											{
+												echo "<br>";
+											}
+											$count = $count + 1;
+										}
+									}
+								}
+								echo "<br>";
+							}
+						}
+					}
+			?>
+		</div>
+		<div class="footer-left"><div class=copyright>&copy; 2019 Rasmus von Schwerdtner</div></div>
+		<div class="footer-right"><div class="link"><a href="./impressum.php">Impressum</a></div></div>
+		</div>
+	</body>
 </html>
