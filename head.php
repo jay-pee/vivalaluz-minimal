d5c7fae8f181bda2edc8137a59b8813d7828548a
Philip <p.janssen@posteo.de>
2019-11-03 13:22:44 +0100

    Initial Commit

---
+++ head.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+    <body>
+    
+        <table cellpadding="5" align="center">
+            <tr>
+                <td>
+                    <h1> vivalaluz.net </h1>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h2> Riva del Garda Carp'it all Club 09/10/2019 </h2> 
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+	<head>
+		<title>Viva La Luz</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<frameset rows="40,*,30" border="0" frameborder="no" framespacing="0">
+
+		<frame name="top" src="head.php" scrolling="no" marginwidth="0" marginheight="0">
+		<frame name="main" src="main.php" marginwidth="0" marginheight="0">
+		<frame name="bottom" src="bottom.php" scrolling="no" marginwidth="0" marginheight="0">
+		<noframes>
+
+			<body></body>
+
+		</noframes>
+	</frameset>
+
+</html>
